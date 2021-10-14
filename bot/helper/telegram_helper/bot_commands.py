3fcc50a26cb6341daee68e715d0bfd35e9cd2401
gigameher473 <84778567+gigameher473@users.noreply.github.com>
2021-10-14 18:01:37 +0530

    Add files via upload

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,31 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.MirrorCommand = 'mirror'
+        self.UnzipMirrorCommand = 'unzipmirror'
+        self.ZipMirrorCommand = 'zipmirror'
+        self.CancelMirror = 'cancel'
+        self.CancelAllCommand = 'cancelall'
+        self.ListCommand = 'list'
+        self.StatusCommand = 'status'
+        self.AuthorizeCommand = 'authorize'
+        self.UnAuthorizeCommand = 'unauthorize'
+        self.PingCommand = 'ping'
+        self.RestartCommand = 'restart'
+        self.StatsCommand = 'stats'
+        self.SpeedCommand = 'speedtest'
+        self.HelpCommand = 'help'
+        self.LogCommand = 'log'
+        self.CloneCommand = "clone"
+        self.WatchCommand = 'watch'
+        self.ZipWatchCommand = 'zipwatch'
+        self.deleteCommand = 'del'
+        self.UsageCommand = 'usage'
+        self.SpeedCommand = 'speedtest'
+        self.LeechSetCommand = 'leechset'
+        self.SetThumbCommand = 'setthumb'
+        self.LeechCommand = 'leech'
+        self.ZipLeechCommand = 'zipleech'
+        self.UnzipLeechCommand = 'unzipleech'
+
+BotCommands = _BotCommands()
